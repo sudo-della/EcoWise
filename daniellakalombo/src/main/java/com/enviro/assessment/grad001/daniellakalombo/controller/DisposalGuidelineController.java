f57b3a4c3712655054a25251e3034d3059167a37
mbelu kalombo <mbkalombo022@student.wethinkcode.co.za>
2025-01-23 22:19:15 +0200

    adding a controller for Disposal Guideline

--- daniellakalombo/src/main/java/com/enviro/assessment/grad001/daniellakalombo/controller/DisposalGuidelineController.java
+++ daniellakalombo/src/main/java/com/enviro/assessment/grad001/daniellakalombo/controller/DisposalGuidelineController.java
@@ -1,58 +1,53 @@
 package com.enviro.assessment.grad001.daniellakalombo.controller;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import com.enviro.assessment.grad001.daniellakalombo.model.DisposalGuidelineModel;
+import com.enviro.assessment.grad001.daniellakalombo.model.WasteCategoryModel;
+import com.enviro.assessment.grad001.daniellakalombo.service.DisposalGuidelineService;
+import com.enviro.assessment.grad001.daniellakalombo.service.WasteCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Getter
-@Entity
-@Table
+import java.util.List;
 
+@RestController
+@RequestMapping("/api/guideline")
+public class DisposalGuidelineController {
 
-public class DisposalGuidelineModel {
+    @Autowired
+    private DisposalGuidelineService disposalGuidelineService;
 
-    @Setter
-    @Id
-    @SequenceGenerator(
-            name = "model_sequence",
-            sequenceName = "model_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "model_sequence"
-    )
-
-    @Column(columnDefinition = "int8")
-    private Long id;
-    @Setter
-    private String category;
-    private String guidelines;
-
-    public DisposalGuidelineModel() {
+    @GetMapping
+    public ResponseEntity<List<DisposalGuidelineModel>> getAllCategories() {
+        return ResponseEntity.ok(disposalGuidelineService.getAllGuidelines());
     }
 
-    public DisposalGuidelineModel(Long id, String category, String guidelines) {
-        this.id = id;
-        this.category = category;
-        this.guidelines= guidelines;
+    @GetMapping("/{id}")
+    public ResponseEntity<DisposalGuidelineModel> getCategoryById(@PathVariable Long id) {
+        DisposalGuidelineModel category = disposalGuidelineService.getGuidelineById(id);
+        if (category != null) {
+            return ResponseEntity.ok(category);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    public DisposalGuidelineModel(String category, String guidelines) {
-        this.category = category;
-        this.guidelines = guidelines;
+    @PostMapping
+    public ResponseEntity<DisposalGuidelineModel> createCategory(@RequestBody DisposalGuidelineModel category) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(disposalGuidelineService.createGuideline(category));
     }
 
-    public void setGuidelines(String guidelines) {
-        this.category = guidelines;
+    @PutMapping("/{id}")
+    public ResponseEntity<DisposalGuidelineModel> updateCategory(@PathVariable Long id, @RequestBody DisposalGuidelineModel category) {
+        category.setId(id); // Set the ID explicitly
+        DisposalGuidelineModel updatedCategory = disposalGuidelineService.updateGuideline(category);
+        return ResponseEntity.ok(updatedCategory);
     }
 
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", category='" + category + '\'' +
-                ", guidelines='" + guidelines + '\'' +
-                '}';
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteGuideline(@PathVariable Long id) {
+        disposalGuidelineService.deleteGuideline(id);
+        return ResponseEntity.noContent().build();
     }
 }
