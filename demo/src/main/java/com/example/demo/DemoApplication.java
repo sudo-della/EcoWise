b8f00b60d40fefbc48b6d8749662a5cef7208082
mbelu kalombo <mbkalombo022@student.wethinkcode.co.za>
2025-01-23 16:39:41 +0200

    creating a skeleton

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+//import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
-@RestController
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
@@ -15,9 +16,6 @@ public class DemoApplication {
 
 
 	}
-	@GetMapping
-	public String hello(){
-		return "Hello World";
-	}
+
 
 }
